614a6ac4b9dc94dc815847893d8daf5c8284df51
ram4ik <ram4ik@hotmail.com>
2023-10-04 20:46:59 +0300

    Initial Commit

---
+++ LoadingViewAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoadingViewAnimation
+//
+//  Created by admin on 04.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadingViewAnimation/LoadingViewAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingViewAnimationApp.swift
+//  LoadingViewAnimation
+//
+//  Created by admin on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoadingViewAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
